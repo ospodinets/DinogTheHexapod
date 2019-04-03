36e0f83eda710fdbbc5b10805899690464f061c6
Oleg Spodinets <oleg.spodinets@gmail.com>
2019-04-03 23:03:22 +0300

    Initial Commit

---
+++ Dinog/Dinog.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Dinog.ino
+ Created:	03-Apr-19 22:39:37
+ Author:	oleg.spodinets
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
